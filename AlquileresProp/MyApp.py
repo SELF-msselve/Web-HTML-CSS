1297a825427a5f1b8e92f32616e2b63cb4755ff1
FARINA SAINT SELVE <RY09678@grupo.ypf.com>
2024-07-12 16:36:53 -0300

    1

--- AlquileresProp/MyApp.py
+++ AlquileresProp/MyApp.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
  
-#df = pd.read_csv('https://raw.githubusercontent.com/SELF-msselve/Web-HTML-CSS/main/AlquileresProp/prop.csv')
-df = pd.read_csv('prop.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/SELF-msselve/Web-HTML-CSS/main/AlquileresProp/prop.csv')
+#df = pd.read_csv('prop.csv')
+
 for index, row in df.iterrows():
     my_expander = st.expander(row['Direccion'], expanded=False)
     with my_expander:
